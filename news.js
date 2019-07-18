e805dbe36f71809c8aa41c016c443afc622078b1
dhruv <dhruvsr4@gmail.com>
2019-07-18 21:24:25 +0530

    removed model.js

--- news.js
+++ news.js
@@ -12,21 +12,15 @@ class News {
 		this.displayPage();
 	}
 
-	//variable used to store the popup content
-
-
 	//Function to display the selected news category
 	selectCategoryNews = () => {
-
-		// console.log(data1);
 		let selectedCategory = document.getElementById('sel-category').value;
-		let display = " ";
+		let display = ' ';
 		let indexDisplay = [];
 		let data = this.data1;
 
-		let selectedData = data.filter(function (e, index) {
-			if (selectedCategory === e.source.name)
-				indexDisplay.push(index);
+		let selectedData = data.filter(function(e, index) {
+			if (selectedCategory === e.source.name) indexDisplay.push(index);
 			return selectedCategory === e.source.name;
 		});
 
@@ -52,13 +46,12 @@ class News {
 		document.getElementById('displaynews').innerHTML = display;
 		for (let index = 0; index < selectedData.length; index++) {
 			console.log(document.getElementById(`myBtn${index}`).innerHTML);
-			document.getElementById(`myBtn${index}`).addEventListener("click", function () {
+			document.getElementById(`myBtn${index}`).addEventListener('click', function() {
 				console.log(data);
 				showpopup(indexDisplay[index], data);
 			});
 		}
-
-	}
+	};
 
 	//On Load Function Call
 
@@ -67,20 +60,20 @@ class News {
 		this.footer();
 		this.displayForm();
 		this.displayNews();
-	}
+	};
 
 	header = () => {
 		let html = `<h1 class="header__heading">NEWSFEED</h1><p class="header__caption">Yet another newsfeed</p>
 				<input type='text' id='subscribe' class='form__subscribe-textbox' placeholder='Email Address'/>
 				<button class='form__subscribe-button' id='subBtn'>Subscribe</button>`;
 		document.getElementById('header').innerHTML = html;
-		document.getElementById('subBtn').addEventListener("click", this.validate);
-	}
+		document.getElementById('subBtn').addEventListener('click', validate);
+	};
 
 	footer = () => {
 		let html = `<p class="footer__copyright">&copy; NewsFeed 2019</p>`;
 		document.getElementById('footer').innerHTML = html;
-	}
+	};
 
 	displayForm = () => {
 		let url = 'https://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=9fdb04ee4078412b82f9dd7f760464f8';
@@ -92,7 +85,7 @@ class News {
 			this.data1.forEach((e) => {
 				channels.push(e.source.name);
 			});
-			channels = [...new Set(channels)];
+			channels = [ ...new Set(channels) ];
 
 			let formDivision = document.createElement('div');
 			formDivision.setAttribute('id', 'iamform');
@@ -112,10 +105,9 @@ class News {
 		</select>
 
 		</div>`;
-			document.getElementById('sel-category').addEventListener("change", this.selectCategoryNews);
+			document.getElementById('sel-category').addEventListener('change', this.selectCategoryNews);
 		});
-
-	}
+	};
 
 	displayNews = () => {
 		let url = 'https://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=9fdb04ee4078412b82f9dd7f760464f8';
@@ -150,40 +142,38 @@ class News {
 			// let newsObj = new News();
 			// document.getElementById(`myBtn0`).addEventListener("click", function () { showpopup(0); });
 			for (let index = 0; index < data.length; index++) {
-				document.getElementById(`myBtn${index}`).addEventListener("click", function () {
+				document.getElementById(`myBtn${index}`).addEventListener('click', function() {
 					showpopup(index, data);
 				});
 			}
 		});
-	}
-
-	//For Validation of Email Address
-	validate = () => {
-		if (/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(document.getElementById('subscribe').value)) {
-			let loc = localStorage.getItem('iamkey');
-			if (loc) {
-				email = JSON.parse(loc);
-			}
-			email.push(document.getElementById('subscribe').value);
-			localStorage.setItem('iamkey', JSON.stringify(email));
-			console.log(localStorage.getItem('iamkey'));
-			alert('You have entered a valid email address!');
-			return true;
-		}
-		alert('You have entered an invalid email address!');
-		return false;
-	}
-
+	};
 }
 
+//For Validation of Email Address
+let validate = () => {
+	if (/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(document.getElementById('subscribe').value)) {
+		let loc = localStorage.getItem('iamkey');
+		if (loc) {
+			email = JSON.parse(loc);
+		}
+		email.push(document.getElementById('subscribe').value);
+		localStorage.setItem('iamkey', JSON.stringify(email));
+		console.log(localStorage.getItem('iamkey'));
+		alert('You have entered a valid email address!');
+		return true;
+	}
+	alert('You have entered an invalid email address!');
+	return false;
+};
 
 //When the user clicks the button, open the modal (for one news channel)
-var showpopup = (i, data) => {
+let showpopup = (i, data) => {
 	// console.log("in showpopup");
 	let modalData = `<div id="myModal" class="modal">
 					<div class="modal-content">
 						<div class="modal-header">
-							<span class="close" onclick="closepopup()">&times;</span>
+							<span class="close" id="spanClose">&times;</span>
 							<h2 id="popup_head"></h2>
 						</div>
 						<div class="modal-body">
@@ -200,24 +190,23 @@ var showpopup = (i, data) => {
 	let contentDivision = document.getElementById('content');
 	contentDivision.appendChild(modelDivision);
 	document.getElementById('iammodal').innerHTML = modalData;
+	document.getElementById('spanClose').addEventListener('click', closepopup);
 	// let newsObj = new News();
 	console.log(data);
 	document.getElementById('popup_head').innerHTML = data[i].title;
 	document.getElementById('popup_content').innerHTML = data[i].content;
 	let modal = document.getElementById('myModal');
 	modal.style.display = 'block';
-
-}
+};
 
 //When the user clicks on <span> (x), close the modal
-var closepopup = () => {
+let closepopup = () => {
 	let modal = document.getElementById('myModal');
 	modal.style.display = 'none';
-}
-
+};
 
 //When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = function(event) {
 	let modal = document.getElementById('myModal');
 	if (event.target == modal) {
 		modal.style.display = 'none';
